de2a5700cad5b87768d24c4f132e6efddc61fe82
octavio-sosa <octavio@ku.edu>
2020-10-21 03:53:37 -0500

    move paddle using movePaddle()

--- src/paddle.js
+++ src/paddle.js
@@ -13,7 +13,20 @@ class Paddle {
 	update(dt) {
 		this.width = cvs.width*0.125
 		this.height = cvs.height*0.015
+		this.movePaddle()
 
+	}
+
+	draw(ctx) {
+		ctx.fillStyle = '#2b0719'
+		if(this.type == 'floor' || this.type == 'ceiling') {
+			ctx.fillRect(this.pos.x, this.pos.y, this.width, this.height)
+		} else if (this.type == 'leftWall' || this.type == 'rightWall') {
+			ctx.fillRect(this.pos.x, this.pos.y, this.height, this.width)
+		}
+	}
+
+	movePaddle() {
 		if(this.type == 'floor') {
 			this.pos.x = mouse.x - this.width/2 - 10
 			this.pos.y = cvs.height-this.height
@@ -26,15 +39,6 @@ class Paddle {
 		} else if (this.type == 'rightWall') {
 			this.pos.x = cvs.width - this.height
 			this.pos.y = mouse.y - this.width/2
-		}
-	}
-
-	draw(ctx) {
-		ctx.fillStyle = '#2b0719'
-		if(this.type == 'floor' || this.type == 'ceiling') {
-			ctx.fillRect(this.pos.x, this.pos.y, this.width, this.height)
-		} else if (this.type == 'leftWall' || this.type == 'rightWall') {
-			ctx.fillRect(this.pos.x, this.pos.y, this.height, this.width)
 		}
 	}
 
