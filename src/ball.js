40ed9a38544748ea108fcc5ab207eaa240a8dd3f
octavio-sosa <octavio@ku.edu>
2020-10-27 13:05:41 -0500

    add collision check to ball.js (faulty)

--- src/ball.js
+++ src/ball.js
@@ -6,6 +6,7 @@ class Ball {
       x: 0,
       y: 0
     }
+    this.velScale = 0.000001
     this.vel = {
       x: 0,
       y: 0
@@ -14,13 +15,22 @@ class Ball {
   }
 
   update(dt) {
-    this.radius = cvs.width*cvs.height*0.000005
+    //this.radius = cvs.width*cvs.height*0.000005
+    this.radius = cvs.width*cvs.height*0.000001
 
     if(this.game.state === GSTATE.TEE) {
       this.tee()
     } else if(this.game.state = GSTATE.TEE_OFF) {
       this.roam()
     }
+
+    this.game.bricks.forEach( brick => {
+      if( this.isCollision(brick)) {
+        console.log("hit!")
+        brick.isHit = true //TODO: remove from gameObjs
+        //repel(this, brick) //updates this.vel TODO
+      }
+    })
   }
 
   draw(ctx) {
@@ -39,10 +49,66 @@ class Ball {
   }
 
   roam() {
-    this.vel.x = cvs.width*cvs.height*0.00001
-    this.vel.y = cvs.width*cvs.height*0.00001
+    this.vel.x = cvs.width*cvs.height*this.velScale
+    this.vel.y = cvs.width*cvs.height*this.velScale
 
     this.pos.x += this.vel.x
     this.pos.y -= this.vel.y
   }
+
+  isCollision(object) {
+    let objSize = {
+      width: object.width,
+      h: object.height
+    }
+
+    let objPos = {
+      start: (object.pos.x, object.pos.y),
+      end: (object.pos.x+objSize.width, object.pos.y+objSize.height),
+    }
+
+    let ballTan = {
+      upper: this.pos.y-this.radius,
+      lower: this.pos.y+this.radius,
+      left: this.pos.x-this.radius,
+      right: this.pos.x+this.radius
+    }
+
+    let objSide = {
+      upper: objPos.start[1],
+      lower: objPos.end[1],
+      left: objPos.start[0],
+      right: objPos.end[0]
+    }
+
+    /*
+    let isInWidthSpan = this.pos.x+this.radius >= objPos.start[0] &&
+
+    let isInWidthSpan = this.pos.x+this.radius >= objPos.start[0] &&
+                    this.pos.x-this.radius <= objPos.end[0]
+
+    let isInHeightSpan = this.pos.y+this.radius >= objPos.start[1] &&
+                     this.pos.y-this.radius <= objPos.end[1]
+    
+    console.log("isInWidthSpan: ", isInWidthSpan)
+    console.log("isInHeightSpan: ", isInHeightSpan)
+    */
+
+    if(ballTan.right >= objSide.left &&
+      ballTan.left <= objSide.right &&
+      ballTan.lower <= objSide.upper &&
+      ballTan.upper <= objSide.lower) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  repel(object) {
+    let collisionVect = {
+      start: (this.pos.x, this.pos.y),
+      end: (0,0)
+    }
+  }
 }
+
